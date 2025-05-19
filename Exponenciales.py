503f7cd38127cd0b3dd8b47e9d3052641178b0d5
Rebe HC <161864810+RebecaH4@users.noreply.github.com>
2025-05-19 08:25:46 -0600

    Add files via upload

---
+++ Exponenciales.py
@@ -0,0 +1,13 @@
+
+x = int(input("Ingresa un numero "))
+i = 0
+m = 0
+d = 0
+for i in range(1, x+1):
+    m = (i**i)*i
+    print(f"{i}  ^   {i+1}  =  {m}")
+    d+=m
+
+print(f"{d} / {x} = {d/x}")
+    
+    

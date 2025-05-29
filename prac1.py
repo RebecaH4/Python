638aec737f2415b7d2769f503c09e7e6fd550ac3
Rebe HC <161864810+RebecaH4@users.noreply.github.com>
2025-05-29 11:51:21 -0600

    Add files via upload

---
+++ prac1.py
@@ -0,0 +1,16 @@
+#Imprimir
+print ("Hola mundo")
+
+#Sumar numeros enteros
+N1 = int(input("Dame un numero "))
+N2 = int(input("Dame otro numero "))
+r = N1 + N2
+
+print ("El resultado es ", r)
+
+#Sumar numeros decimales
+n1 = float(input("Dame un valor "))
+n2 = float(input("Dame otro valor "))
+suma = n1 + n2
+
+print ("El resultado es ", suma)
